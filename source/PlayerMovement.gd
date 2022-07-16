2723d07b1b7be50067b07e503cb541b00f25db2a
Salixe1214 <alexis.stamand.1@gmail.com>
2022-07-16 13:50:23 -0400

    + jumping

--- source/PlayerMovement.gd
+++ source/PlayerMovement.gd
@@ -2,6 +2,7 @@ extends KinematicBody
 
 export var speed = 14
 export var fall_acceleration = 75
+export var jump_impulse = 20
 
 var mouse_sens = 0.3
 var camera_anglev = 0
@@ -12,6 +13,7 @@ var velocity = Vector3.ZERO
 func _physics_process(delta):
 	var direction = Vector3.UP
 	
+	# Moving
 	if Input.is_action_pressed("move_right"):
 		direction += $Pivot.global_transform.basis.x
 	if Input.is_action_pressed("move_left"):
@@ -28,6 +30,10 @@ func _physics_process(delta):
 	velocity.z = direction.z * speed
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	# Jumping
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_impulse
 
 
 func _input(event):   

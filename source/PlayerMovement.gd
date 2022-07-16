bf20ddbd442ae46eb133cc947c3fac827c281ec9
Salixe1214 <alexis.stamand.1@gmail.com>
2022-07-16 12:42:02 -0400

    * movement working

--- source/PlayerMovement.gd
+++ source/PlayerMovement.gd
@@ -4,22 +4,22 @@ export var speed = 14
 export var fall_acceleration = 75
 
 var mouse_sens = 0.3
-var camera_anglev=0
+var camera_anglev = 0
 
 var velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-
+	var direction = Vector3.UP
+	
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
+		direction += $Pivot.global_transform.basis.x
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
+		direction -= $Pivot.global_transform.basis.x
+	if Input.is_action_pressed("move_backward"):
+		direction += $Pivot.global_transform.basis.z
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		direction -= $Pivot.global_transform.basis.z
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
@@ -29,10 +29,13 @@ func _physics_process(delta):
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
 
-func _input(event):         
+
+func _input(event):   
+	print()
+		  
 	if event is InputEventMouseMotion:
 		$Pivot.rotate_y(deg2rad(-event.relative.x*mouse_sens))
 		var changev=-event.relative.y*mouse_sens
 		if camera_anglev+changev>-50 and camera_anglev+changev<50:
 			camera_anglev+=changev
-			$Pivot.rotate_x(deg2rad(changev))
+			$Pivot/Camera.rotate_x(deg2rad(changev))

fd21d7ea255dea9b8916d16612ad5c8e4d3994f9
Salixe1214 <alexis.stamand.1@gmail.com>
2022-07-29 04:42:50 -0400

    * Modular HotBar

--- source/Scripts/ActorComponents/Player/Inventory.gd
+++ source/Scripts/ActorComponents/Player/Inventory.gd
@@ -1,10 +1,11 @@
 extends Control
 
-export var my_images = ["res://Assets/Materials/sword.jpg"]
+export var contents = []
+export var bar_size = 9
 var id = 0
 
-func _physics_process(delta):
-	if (Input.is_action_just_pressed("jump") and is_visible_in_tree()):
-		id = (id + 1) % my_images.size()
-		print(id)
-		$GridContainer/TextureRect/TextureRect.texture = load(my_images[id])
+func _ready():
+	for i in range(bar_size):
+		var slot = load("res://Scenes/UI/hb_slot.tscn").instance()
+		slot.name = str(i)
+		$GridContainer.add_child(slot)

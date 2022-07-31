15b3c182185d646b193bf63a3b62a7b343a2745f
Maxime Morin Grandmont <maxime.mg.11@gmail.com>
2022-07-31 15:53:39 -0400

    Tool Data
    + Added new temp assets
    + Added ToolData
    * Changed the default test item

--- source/Scripts/Data/ToolData.gd
+++ source/Scripts/Data/ToolData.gd
@@ -1,8 +1,8 @@
 extends ItemData
 class_name ToolData
 
-export(int) var DamagePoints;
-export(float) var AttackRange;
+export(int) var DamagePoints = 1;
+export(float) var AttackRange = 1.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():

dbaf7e9748310688f2b80223738039181b78f7f3
Maxime MG <maxime.mg.11@gmail.com>
2022-08-09 02:03:07 -0400

    ! WARNING, CRASHES, COMMENT LINE 28 + First prototype for dynamic item table creation.

--- source/Scripts/Utility/ItemsTable.gd
+++ source/Scripts/Utility/ItemsTable.gd
@@ -1,11 +1,30 @@
-extends Resource
+extends Node
 class_name ItemTable
 
-
-export(Array, Resource) var items
-var table = {}
+export(String) var path
 
 func _init():
-	for item in items:
-		table[item.ID] = item
+	load_itemData_from_dir("res://Data/ItemData")
+	pass
+
+func load_itemData_from_dir(var dirPath):
+	var dir = Directory.new()
+	if dir.open(dirPath) == OK:
+		dir.list_dir_begin()
+		
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				pass
+				#load_itemData_from_dir(file_name)
+			else:
+				load_itemData_from_file(file_name)
+			file_name = dir.get_next()
+		
+	else:
+		print("An error occurred when trying to access the path.")
+		
+func load_itemData_from_file(var fileName):
+	var itemResource = ResourceLoader.load("res://Data/ItemData/" + fileName, "tres", true)
+	print("Found item!")
 

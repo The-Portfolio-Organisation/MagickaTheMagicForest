5fe3503c0e4991719375b86c94ffc14ceefd1e10
Maxime Morin Grandmont <maxime.mg.11@gmail.com>
2022-08-15 23:57:32 -0400

    A kinda working item factory?

--- source/Scripts/Data/ItemResource.gd
+++ source/Scripts/Data/ItemResource.gd
@@ -1,9 +1,9 @@
 extends Resource
-class_name ItemResource
 
 export var ID = 0;
-export(String) var DisplayName = "Test_Item"
+export(String) var DisplayName = "DISPLAY_NAME"
 export(Mesh) var RefModel;
+export(Shape) var RefCollider = CapsuleShape.new()
 export(StreamTexture) var RefUIImage;
 
 var itemType = EItemType.Default;
@@ -11,7 +11,7 @@ var itemType = EItemType.Default;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- source/Scripts/Utility/Item/ItemFactory.gd
+++ source/Scripts/Utility/Item/ItemFactory.gd
@@ -1,20 +1,27 @@
 extends Node
 
-export(NodePath) var itemSubscene
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#var itemSubscenePath = "res://Subobjects/Item.tscn"
 
+var itemSubscene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	itemSubscene = preload("res://Subobjects/Item.tscn")
 	pass # Replace with function body.
 
 func make_item(var itemData):
-	if itemData is ItemResource:
-		pass
-	else:
-		return null
+	# Edge case
+	# Apparently gdscript can't check for custom classes
+	#if !(itemData is ItemResource):
+		#return null
+	
+	var itemSubobject = itemSubscene.instance()
+	var itemRoot = itemSubobject.get_node("./")
+	itemRoot.set_item_resource_data(itemData)
+	
+	get_tree().current_scene.add_child(itemSubobject)
+	
+	return itemSubobject
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
